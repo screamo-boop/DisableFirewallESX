3baaa92214ad04c92e035aa9a69a73681777f687
Danil <64461997+screamo-boop@users.noreply.github.com>
2020-05-10 18:41:15 +0500

    Add files via upload

---
+++ DisableFirewallESXI.ps1
@@ -0,0 +1,26 @@
+#Script to disable firewall using PowerCLI
+#Install PowerCLI and set correct execution policy if not work
+# 1. Initialize vars and configure PowerCLI
+Set-PowerCLIConfiguration -DefaultVIServerMode Multiple     `
+                          -InvalidCertificateAction Ignore  `
+                          -Confirm:$false
+$esxuser = 'root'
+$esxpass = 'P@ssw0rd'
+# 2. read stands.txt file and pognali
+$stands = Get-Content stands.txt
+foreach ($stand in $stands) {
+    #Connect
+    Connect-VIServer -Server $stand -User $esxuser -Password $esxpass
+    #Get existing policy
+    $policy = Get-VMHostFirewallDefaultPolicy
+    #Replace existing policy
+    Set-VMHostFirewallDefaultPolicy -Policy $policy -AllowIncoming $true -AllowOutgoing $true
+    #Start VMs and answer question
+    $vms = Get-VM | Where-Object { $_.PowerState -eq 'PoweredOff' }
+    foreach ($vm in $vms) {
+        Get-VM $vm | Start-VM | Get-VMQuestion | Set-VMQuestion -DefaultOption 
+    }
+    #Disconnect
+    Disconnect-VIServer -Confirm:$false
+    #repeat
+}
